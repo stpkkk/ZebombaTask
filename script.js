f4a0ff0a1f3476b7dcef1ab4a49c16e97458c270
Igor Stepanov <stipyk1309@gmail.com>
2025-02-02 14:46:18 +0300

    refactor: CharacterController

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
 'use strict';
 
-const character = document.querySelector('.character');
-const moveButton = document.querySelector('.moveButton');
-
 class CharacterController {
   constructor(characterElement, totalSteps) {
     this.character = characterElement;
     this.totalSteps = totalSteps;
-    this.currentLocation = new Array(totalSteps).fill(0);
-    this.currentLocation[0] = 1;
+    this.currentPosition = 0;
   }
 
   moveToNextPoint() {
-    const currentPosition = this.currentLocation.indexOf(1);
-
-    if (currentPosition === -1) return;
-
-    if (currentPosition >= this.totalSteps - 1) {
-      this.resetPosition();
-      return;
-    }
-
-    this.updatePosition(currentPosition, currentPosition + 1);
-    this.updateCharacterClass(currentPosition + 1);
+    this.currentPosition = (this.currentPosition + 1) % this.totalSteps;
+    this.updateCharacterClass();
   }
 
-  resetPosition() {
-    this.updatePosition(this.currentLocation.indexOf(1), 0);
-    this.updateCharacterClass(0);
-  }
-
-  updatePosition(from, to) {
-    this.currentLocation[from] = 0;
-    this.currentLocation[to] = 1;
-  }
-
-  updateCharacterClass(step) {
-    this.character.className = `character step-${step}`;
+  updateCharacterClass() {
+    this.character.className = `character step-${this.currentPosition}`;
   }
 }
 
+const character = document.querySelector('.character');
+const moveButton = document.querySelector('.moveButton');
 const characterController = new CharacterController(character, 11);
 
 moveButton.addEventListener('click', () =>
