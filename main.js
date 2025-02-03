236870e2c058eb4deceb32a323b747af6ee1a688
Igor Stepanov <stipyk1309@gmail.com>
2025-02-03 17:09:46 +0300

    feat: rating modal layout and functional implement

--- main.js
+++ main.js
@@ -115,3 +115,76 @@ new Slider(data);
 moveButton.addEventListener('click', () =>
   characterController.moveToNextPoint()
 );
+
+//Rating
+document.addEventListener('DOMContentLoaded', () => {
+  const ratingButton = document.querySelector('.rating');
+  const main = document.querySelector('main');
+
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  const ratingWindow = document.createElement('div');
+  ratingWindow.classList.add('rating-window');
+  ratingWindow.innerHTML = `
+    <button class="close-button"><img src='./images/buttons/close-button.png' alt='close'/></button>
+    <h2 class="rating-modal-header">Рейтинг игроков</h2>
+    <div class="rating-table-container">
+      <table class="rating-table">
+        <thead>
+          <tr>
+            <th>Место</th>
+            <th></th>
+            <th>Имя Фамилия</th>
+            <th>Опыт</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  `;
+
+  main.appendChild(overlay);
+  main.appendChild(ratingWindow);
+
+  const tbody = ratingWindow.querySelector('tbody');
+
+  function populateTable() {
+    let playerPlace = 1;
+    const sortedData = data.rating.sort((a, b) => b.points - a.points);
+
+    tbody.innerHTML = '';
+    sortedData.forEach(player => {
+      const isFriend = data.friends.some(friend => friend.id === player.id);
+      const row = document.createElement('tr');
+      if (isFriend) row.classList.add('friend-highlight');
+      row.innerHTML = `
+         <td>${playerPlace++}</td>
+        <td><img class='player-image' width={26} height={26} src=${
+          player.img
+        } alt=${player.name}/></td>
+        <td>${player.name} ${player.lastName}</td>
+        <td>${player.points}</td>
+      `;
+      tbody.appendChild(row);
+    });
+  }
+
+  function toggleRatingWindow() {
+    const isActive = ratingWindow.classList.contains('active');
+    if (isActive) {
+      ratingWindow.classList.remove('active');
+      overlay.classList.remove('active');
+    } else {
+      populateTable();
+      ratingWindow.classList.add('active');
+      overlay.classList.add('active');
+    }
+  }
+
+  ratingButton.addEventListener('click', toggleRatingWindow);
+  overlay.addEventListener('click', toggleRatingWindow);
+  ratingWindow
+    .querySelector('.close-button')
+    .addEventListener('click', toggleRatingWindow);
+});
